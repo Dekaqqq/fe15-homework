bc7c84f168d1dfecdab27fa0ba576aef06223d12
dekaqqq <kdrushlyak@gmail.com>
2019-04-23 18:02:29 +0300

    fix

--- js/module-02/js/script.js
+++ js/module-02/js/script.js
@@ -8,21 +8,19 @@ let total = 0;
 while (true) {
     const input = prompt('Enter number');
     if (input === null) break; 
-    else if (isNaN(input) || input === '') {
+    else if (Number.isNaN(Number(input))) {
         alert('Было введено не число, попробуйте еще раз');
     } else {
         numbers.push(Number(input));
     }
 }
 
-if (numbers.length > 0) {
-    for (let i = 0; i < numbers.length; i += 1) {
-    total += numbers[i];
+if (numbers.length !== 0) {
+    for (const number of numbers) {
+        total += number;
     }
+    console.log(`Общая сумма чисел равна ${total}`);
 }
-
-console.log(numbers);
-console.log(`Общая сумма чисел равна ${total}`);
 
 
 // TASK 2
@@ -31,28 +29,21 @@ const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let attemptsLeft = 3;
 
 
-while (true) {
+while (attemptsLeft) {
     const userInput = prompt('Enter password');
     if (userInput === null) break;
-    if (userInput === '') {
-        alert('Enter something');
-    }
     if (passwords.includes(userInput)) {
         alert('Добро пожаловать!');
         break;
-    } else if (!passwords.includes(userInput) && userInput !== '') {
+    } else {
         attemptsLeft -= 1;
-        if (attemptsLeft === 0) {
-            alert('У вас закончились попытки, аккаунт заблокирован!');
-            break;
-        } else {
-            alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
+         if (attemptsLeft === 0){
+         alert(`У вас закончились попытки, аккаунт заблокирован!`);
+         break;
         }
+        alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`)
     }
 }
-
-
-
 
 
 
